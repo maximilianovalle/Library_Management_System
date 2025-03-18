d572a3c19b32e696b6a9b5d2d87bffbb946d86e4
abeaduhh <adiba.labonno@gmail.com>
2025-03-18 02:26:02 -0500

    it dont work but im sleepy

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+// import Account from './pages/Account'
+// import HomePage from './pages/HomePage'
+// import LibrarianPage from './pages/LibrarianPage'
+// import Login from './pages/LoginPage'
+// import OutputPage from './pages/OutputPage'
+// import UserPage from './pages/UserPage'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/OutputPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const OutPutPage = () => {
+    return{
+
+    };
+}
+
+export default OutPutPage
