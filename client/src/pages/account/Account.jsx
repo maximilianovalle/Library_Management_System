a3cb0e023cfaf5c51b9152f589a64f4a2b06604d
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-03-19 15:51:01 -0500

    Add header to account page

--- client/src/pages/account/Account.jsx
+++ client/src/pages/account/Account.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import './Account.css';
+import { useState } from 'react'; // allows us to track data or properties that need tracking in a function component
 
 const Account = () => {
-    return(
-        <div>
-            <h1>Account Page</h1>
+
+    const [email, updateEmail] = useState("");  // [current state, function that updates the state] = set to empty string
+
+    return( // HTML -----
+
+        <div id="body">
+
+            <div id="header">
+                <h1>Cougar Public Library</h1>
+
+                <form>
+                    <input type="text" placeholder="Search ..."/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+
+            <div id="main">
+
+            </div>
+
         </div>
+
     );
 }
 
