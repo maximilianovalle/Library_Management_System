0c2a7df4c6cd727a60be588abbee99d1d10ac88c
alan <alanitoyah96@gmail.com>
2025-03-19 17:06:24 -0500

    Split header file into react / css file for ease

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <nav className="nav">
+                    <div className="logo">
+                        <a href="/">
+                            <img src="/logo.png" alt="Logo" />
+                        </a>
+                        <h1>Cougar Public Library</h1>
+                    </div>
+
+                    <div className={`nav-links`}>
+                        {["My Books", "Browse Books", "Browse Devices"].map((item) => (
+                            <a key={item} href="/" className="link">
+                                {item}
+                            </a>
+                        ))}
+                        <button className="button">Log in</button>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
