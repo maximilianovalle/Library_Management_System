c6d0a7c92c9b97576a4f2ee2f3858bed04fd40f6
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-03-19 17:22:21 -0500

    Comment code

--- client/src/pages/account/Account.jsx
+++ client/src/pages/account/Account.jsx
@@ -4,21 +4,26 @@ import { useState } from 'react'; // allows us to track data or properties that 
 
 const Account = () => {
 
-    const [email, updateEmail] = useState("");  // [current state, function that updates the state] = set to empty string
+    // [current state, function that updates the state] = set to empty string
+    const [search, submitSearch] = useState("");
+    const [email, updateEmail] = useState(""); 
 
     return( // HTML -----
 
         <div id="body">
 
+            {/* top header */}
             <div id="header">
                 <h1>Cougar Public Library</h1>
 
+                {/* search bar */}
                 <form>
-                    <input type="text" placeholder="Search ..."/>
+                    <input type="text" placeholder="Search ..." value={ search } onChange={ (e) => submitSearch(e.target.value) }/>
                     <button type="submit">Submit</button>
                 </form>
             </div>
 
+            {/* main content */}
             <div id="main">
 
             </div>
