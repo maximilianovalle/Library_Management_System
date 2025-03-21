0a194a1c090de66f36088ec5f0b41897bf49355a
abeaduhh <adiba.labonno@gmail.com>
2025-03-20 20:57:25 -0500

    must work on other comp

--- client/src/pages/login/LoginPage.jsx
+++ client/src/pages/login/LoginPage.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import './LoginPage.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
+
+
 const Login = () => {
     const [userID, setUserID] = useState('');
     const [password, setPassword] = useState("");
+    const [message, setMessage] = useState('');
 
+
+    const handleSubmit = async () => {
+        try {
+            const response =
+            await axios.post('http://localhost:8000/login', { userID, password }).then((response) => {
+                console.log(response);
+            }
+            );
+            if (response.status === 200) {
+                setMessage('Login successful');
+            }
+        }
+        catch (error) {
+            console.log(error);
+            setMessage('Invalid user ID or password');
+        }
+    };
     return(
         <div className="login">
             <h1>Welcome to Cougar Public Library</h1>
@@ -35,7 +55,8 @@ const Login = () => {
                             required
                         />
                     </div>
-                    <button className= "login_button">Login</button>
+                    <button className= "login_button" onClick= {handleSubmit}>Login</button>
+                    {message && <p>{message}</p>}
                     
                 </div>
             </form>
--- server/server.js
+++ server/server.js
@@ -1,17 +1,34 @@
-var http = require("http");
+var http = require('http');
 const pool = require("./database.js");   
-
+const cors = require('cors');
 const login = require("./login_function/login.js");
+const { log } = require('console');
 
-const app = http.createServer(function (req, res) {                                // create the server
-    res.write('Starting a base!');
+const app = http.createServer( async (req, res) => {                                // create the server
     
-}).listen(8000);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
+    if (req.method === 'OPTIONS') {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'backend' }));
+
+    if(req.url === '/login' && req.method === 'POST') {
+        login(req, res);
+    }
+    res.end();
+}).listen(8000, console.log('Server is running on port 8000'));
 
 
-app.post("/login", (req,res) => {
-    login(req,res);
-})
+// app.post("/login", (req,res) => {
+//     login(req,res);
+// })
 // async function getLibrarianInfo() {
 //     const [rows] = await pool.query('SELECT * FROM librarian');
 //     return rows;
