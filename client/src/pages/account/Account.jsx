4782796f608dd4a82f99966a81047e94d3e45fd6
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-03-22 23:24:23 -0500

    Display past books + devices in account and update header

--- client/src/pages/account/Account.jsx
+++ client/src/pages/account/Account.jsx
@@ -4,6 +4,15 @@ import './Account.css';
 
 // The (React) frontend sends a GET, POST, DELETE, etc. request to the (NodeJS) backend. The backend server.js file processes the request and returns a response that is then displayed on the frontend.
 
+
+const items = [
+    {title: "My Books", link: "/mybooks"},
+    {title: "Browse Books", link: "/browsebooks"},
+    {title: "Browse Devices",link: "/browsedevices"},
+    {title: "Account", link: "/account"},
+
+]
+
 const Account = () => {
 
     const [firstName, setFirstName] = useState("");
@@ -11,6 +20,8 @@ const Account = () => {
     const [email, setEmail] = useState("");
     const [createdAt, setCreatedAt] = useState("");
     const [fineAmntDue, setFineAmnt] = useState("");
+    const [pastBooksArray, setPastBooks] = useState("");
+    const [pastDevicesArray, setPastDevices] = useState("");
 
     const [error, setError] = useState("");
 
@@ -21,6 +32,7 @@ const Account = () => {
             try {
                 const res = await axios.get("http://localhost:8000/account");   // sends a GET request to /account
 
+                // receive JSON from account.js
                 setFirstName(res.data.firstName);
                 setLastName(res.data.lastName);
                 setEmail(res.data.email);
@@ -30,6 +42,8 @@ const Account = () => {
                     day: "numeric",
                 }));    // saves date as January 1, 2025 instead of 2025-01-01
                 setFineAmnt(res.data.fineAmntDue);
+                setPastBooks(res.data.pastBooksArray);
+                setPastDevices(res.data.pastDevicesArray);
 
             } catch (error) {
                 console.error("Error fetching account data:", error);
@@ -38,7 +52,7 @@ const Account = () => {
 
         }
 
-        fetchName();    // need to explicitly call fetchName to run it
+        fetchName();    // need to explicitly call fetchName to run
     }, []);
 
     return( // HTML -----
@@ -49,7 +63,7 @@ const Account = () => {
 
             <header className="header">
                 <div className="container">
-                    <nav className="nav">
+                <nav className="nav">
                         <div className="logo">
                             <a href="/">
                                 <img src="/logo.png" alt="Logo" />
@@ -58,11 +72,12 @@ const Account = () => {
                         </div>
 
                         <div className={`nav-links`}>
-                            {["My Books", "Browse Books", "Browse Devices"].map((item) => (
-                                <a key={item} href="/" className="link">
-                                    {item}
+                            {items.map((item) => (
+                                <a key={item.title} href={item.link} className="link">
+                                    {item.title}
                                 </a>
                             ))}
+                            <button onClick={() => {localStorage.clear(); window.location.href = '/login';}} className = "logout_button">Logout</button>
                         </div>
                     </nav>
                 </div>
@@ -92,7 +107,41 @@ const Account = () => {
 
                     <h1>Recent Activity</h1>
 
+                    {/* past books */}
+                    <h2>Past Books</h2>
 
+                    {pastBooksArray.length > 0 ? (  // if user has past checked out books
+                        <ul id="pastBooksList">
+                            {pastBooksArray.map((book, i) => (
+                                <li key={i}>
+                                    {book.title}, {book.author} <br />
+                                    {book.checkoutDate} - {book.returnedDate}
+                                    <p></p>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (   // if user has no past checked out books
+                        <p>No books to display! Try returning a checked out book...</p>
+                    )
+                    }
+
+                    {/* past devices */}
+                    <h2>Past Devices</h2>
+
+                    {pastBooksArray.length > 0 ? (  // if user has past checked out devices
+                        <ul id="pastDevicesList">
+                            {pastDevicesArray.map((device, i) => (
+                                <li key={i}>
+                                    {device.category}, {device.model} <br />
+                                    {device.checkoutDate} - {device.returnedDate}
+                                    <p></p>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (   // if user has no past checked out books
+                        <p>No devices to show! Try returning a checked out device...</p>
+                    )
+                    }
 
                 </div>
 
--- server/account_info/account.js
+++ server/account_info/account.js
@@ -7,9 +7,11 @@ module.exports = async function getUserName(req, res) {
         // TEMPORARY USER ID FOR TESTING
         const TEMP_USER = 7000001;
         
-        const [[userInfo], [fineInfo]] = await Promise.all([
+        const [[userInfo], [fineInfo], [pastBooksRows], [pastDevicesRows]] = await Promise.all([
             pool.query("SELECT First_Name, Last_Name, Email, Created_At FROM user WHERE User_ID = ?", [TEMP_USER]), // queries database for user info of specific User_ID
-            pool.query("SELECT Amount FROM fines WHERE User_ID = ? AND Fine_Status = 2", [TEMP_USER])   // queries database for user unpaid fine amount
+            pool.query("SELECT Amount FROM fines WHERE User_ID = ? AND Fine_Status = 2", [TEMP_USER]),   // queries database for user unpaid fine amount
+            pool.query("SELECT book.Title, author.Name, borrow_record.Checkout_Date, borrow_record.Return_Date FROM borrow_record, book, author WHERE borrow_record.User_ID = ? AND borrow_record.Return_Date IS NOT NULL AND borrow_record.ISBN IS NOT NULL AND book.ISBN = borrow_record.ISBN AND author.Author_ID = book.Author_ID", [TEMP_USER]),  // select all previously checked out + returned books for user
+            pool.query("SELECT Category, Model, Checkout_Date, Return_Date FROM borrow_record WHERE User_ID = ? AND Return_Date IS NOT NULL AND Category IS NOT NULL", [TEMP_USER]),
         ]);
 
         if (userInfo.length === 0) {    // if no user found
@@ -22,13 +24,39 @@ module.exports = async function getUserName(req, res) {
         let firstName = userInfo[0].First_Name;
         let lastName = userInfo[0].Last_Name;
         let email = userInfo[0].Email;
-        let createdAt = userInfo[0].Created_At;
+        let createdAt = userInfo[0].Created_At
 
         let fineAmntDue = 0;
 
-        for (let i = 0; i < fineInfo.length; i++) {
+        for (let i = 0; i < fineInfo.length; i++) { // add up all fine amounts to get total fines due
             fineAmntDue += fineInfo[i].Amount;
         }
+
+        let pastBooksArray = pastBooksRows.map(row => ({    // format pastBooks into array of objects
+            title: row.Title,
+            author: row.Name,
+
+            checkoutDate: row.Checkout_Date
+            ? new Date(row.Checkout_Date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })
+            : "???",    // changes YYYY-MM-DD to Month DD, YYYY
+
+            returnedDate: row.Return_Date
+            ? new Date(row.Return_Date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })
+            : "???",    // changes YYYY-MM-DD to Month DD, YYYY
+        }));
+
+        let pastDevicesArray = pastDevicesRows.map(row => ({
+            category: row.Category,
+            model: row.Model,
+            checkoutDate: row.Checkout_Date
+
+            ? new Date(row.Checkout_Date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })
+            : "???",    // changes YYYY-MM-DD to Month DD, YYYY
+
+            returnedDate: row.Return_Date
+            ? new Date(row.Return_Date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })
+            : "???",    // changes YYYY-MM-DD to Month DD, YYYY
+        }));
 
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({
@@ -37,7 +65,9 @@ module.exports = async function getUserName(req, res) {
             email: email,
             createdAt: createdAt,
             fineAmntDue: fineAmntDue,
-        })); // send firstName, lastName, email, createdAt, fineAmntDue as JSON
+            pastBooksArray: pastBooksArray,
+            pastDevicesArray: pastDevicesArray,
+        })); // send firstName, lastName, email, createdAt, fineAmntDue as JSON to Account.jsx
 
     } catch (error) {
         res.writeHead(500, { 'Content-Type': 'application/json' }); // HTTP 500: Internal Server Error
