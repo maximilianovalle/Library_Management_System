1bd435782bb8922d05e9a3de3bcff7a01c049b84
alan <alanitoyah96@gmail.com>
2025-04-13 22:32:37 -0500

    reports page blueprint

---
+++ client/src/pages/manager/ReportsPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/header/ManagerHeader";
+
+
+const ReportsPage = () => {
+    return(
+        <div>
+            <Header />
+            <h1>Reports</h1>
+        </div>
+    );
+}
+
+export default ReportsPage
