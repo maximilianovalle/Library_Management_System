890aec32fdfe3732ff634e6d27a3570c31188b19
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-04-15 15:37:53 -0500

    Display all non-deleted books

--- client/src/pages/manager/deleteItems.jsx
+++ client/src/pages/manager/deleteItems.jsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import { MdLaptopChromebook } from "react-icons/md";
+import { MdLaptopChromebook, MdModeEdit, MdDeleteForever } from "react-icons/md";
 import { BiLibrary } from "react-icons/bi";
 
+import DropDown from '../browse/components/drop_down';
+import defaultCover from '../checkedOutItems/book-not-found.png';
 import Header from "../../components/header/ManagerHeader";
 import "./deleteItems.css";
 
+const browse_by = ["Title", "ISBN", "Author", "Genre"];
+
+const processBooks = (books) => {
+    const bookMap = new Map();
+
+    books.forEach((book) => {
+        const isbn = book.ISBN;
+
+        if (bookMap.has(isbn)) {
+            bookMap.get(isbn).count += 1;
+        } else {
+            bookMap.set(isbn, { ...book, count: 1 });
+        }
+    });
+
+    return Array.from(bookMap.values());
+};
 
 const ManagerDeleteItems = () => {
 
@@ -15,6 +34,9 @@ const ManagerDeleteItems = () => {
 
     const [activeTab, setActiveTab] = useState("books");
     const [toast, setToast] = useState({ message: "", type: "" });
+
+    const [search_value, setSearchValue] = useState("");
+    const [search_by, setSearchBy] = useState("");
 
 
     const showToast = (message, type = "success") => {
@@ -26,33 +48,60 @@ const ManagerDeleteItems = () => {
         try {
             const token = localStorage.getItem("token");
 
-            const res = await axios.get(`${process.env.REACT_APP_API}/manage_items`, {
-                headers: {"Authorization": `Bearer ${token}`},
+            if (!token) {
+                console.error("No token found. Redirecting to login...");
+                window.location.href = "/login";
+                return;
+            }
+
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/manage_items`, {
+                headers: {Authorization: `Bearer ${token}`},
             });
 
-            setBooks(res.data.books);
-            setDevices(res.data.devices);
+            setBooks(res.data.allBooks);
+            setDevices(res.data.allDevices);
         } catch (error) {
             console.error("Error fetching books: ", error);
         }
     }
 
     useEffect(() => {
+        fetchItems();
+    }, []);
+
+    // book search
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        fetchBooks({
+            search_by: search_by,
+            search_value,
+        });
+    };
+
+    const fetchBooks = async (params = {}) => {
+        console.log(params);
         try {
             const token = localStorage.getItem("token");
-
             if (!token) {
                 console.error("No token found. Redirecting to login...");
                 window.location.href = "/login";
                 return;
             }
-            
-        } catch (error) {
-            console.error("Error fetching info: ", error);
-        }
 
-        fetchItems();
-    }, []);
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/books`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                },
+                params,
+            });
+
+            setBooks(response.data.allBooks);
+        } catch (error) {
+            console.error("Error fetching books:", error);
+        }
+    };
 
     return (
         <div>
@@ -77,9 +126,55 @@ const ManagerDeleteItems = () => {
 
                 {/* books tab */}
 
-                {activeTab === "books" && (
-                    <p>Test</p>
-                )}
+                {activeTab === "books" && (<>
+                    {books.length > 0 ? (<>
+
+                        {/* search bar */}
+
+                        <form className="search fadeInAnimation" onSubmit={handleSearch}>
+                            <div className="dropdown">
+                                <DropDown options={browse_by} value={search_by} onSelect={(selectedOption) => { setSearchBy(selectedOption); }}/>
+                            </div>
+
+                            <input className="search_bar" type="text" label="Search" placeholder="Search Book..." value={search_value} onChange={(e) => setSearchValue(e.target.value)}/>
+                            
+                            <button className="browse_button" type="submit">Enter</button>
+                        </form>
+
+                        {/* books */}
+
+                        <div className="books_container fadeInAmnimation">
+
+                        {books.map((book, index) => {
+                            
+                            return (<>
+                            <div key={index} className="book_card">
+
+                                <h3 className="entryElement">{book.title}</h3>
+
+                                <img className="book_image" src={`https://covers.openlibrary.org/b/isbn/${book.ISBN}-L.jpg?default=false`} alt={book.Title}onError={(e) => {
+                                    e.target.onerror = null;
+                                    e.target.src = defaultCover;
+                                }}/>
+
+                                <p class="entryElement">{book.author}, {book.publication}</p>
+                                <p class="bookGenreSubtext entryElement"><em>{book.genre}</em></p>
+
+                                <div class="bookBtnContainer">
+                                    <button class="bookCardBtn"><MdModeEdit /></button>
+                                    <button class="bookCardBtn"><MdDeleteForever /></button>
+                                </div>
+
+                            </div>
+                            </>);
+                        })}
+
+                        </div>
+
+                    </>) : (
+                        <p>No books found.</p>
+                    )}
+                </>)}
 
                 {/* devices tab */}
 
--- server/manager_page/getManagerBooksDevices.js
+++ server/manager_page/getManagerBooksDevices.js
@@ -1,9 +1,40 @@
 const pool = require('../database.js');
 
-module.exports = async function getManagerBooks(req, res) {
+module.exports = async function getManagerBooksDevices(req, res) {
     try {
-        // gets all unique books that have at least one non-deleted copy (i.e. all books available, checked out, or in maintenance)
-        const [allBooks] = await pool.query(`SELECT DISTINCT book.* FROM book JOIN book_copies ON book.ISBN = book_copies.ISBN WHERE book_copies.Book_Status != 'Deleted'`);
+        // gets all unique books + devices that have at least one non-deleted copy (i.e. all books available, checked out, or in maintenance)
+        const [[allBooks], [allDevices]] = await Promise.all([
+            pool.query(`SELECT DISTINCT book.*, author.Name FROM book JOIN author ON book.Author_ID = author.Author_ID JOIN book_copies ON book.ISBN = book_copies.ISBN WHERE book_copies.Book_Status != 'Deleted'`),
+
+            pool.query(`SELECT DISTINCT device.* FROM device JOIN device_copies ON device.Model = device_copies.Model WHERE device_copies.Device_Status != 'Deleted'`),
+        ]);
+
+        // let uniqueBooks = allBooks.length;
+        // let uniqueDevices = allDevices.length;
+
+        let allBooksArr = allBooks.map(row => ({
+            title: row.Title,
+            genre: row.Genre,
+            ISBN: row.ISBN,
+            publication: row.Publication_Year,
+            author: row.Name,
+        }))
+
+        let allDevicesArr = allDevices.map(row => ({
+            model: row.Model,
+            category: row.Category,
+        }))
+
+        let uniqueBooks = allBooksArr.length;
+        let uniqueDevices = allDevicesArr.length;
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({
+            allBooks: allBooksArr,
+            allDevices: allDevicesArr,
+            uniqueBooks: uniqueBooks,
+            uniqueDevices: uniqueDevices,
+        }))
 
     } catch (error) {
         console.error("Error fetching books: ", error);
